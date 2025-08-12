f69659a1ac0456b763323dc9ff4fb03fe5e0e050
professional-pigeon <kyle.kayperez@gmail.com>
2025-08-12 14:49:36 -0700

    update usePosts to take view type

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 import { blogPosts } from '@/constants/dummyData';
 
 // Mimic an api fetch and set hook
-export default function usePosts() {
+export default function usePosts({ view }) {
   const [blogPostData, setBlogPostData] = useState({ posts: [], isLoaded: false })
 
   useEffect(() => {
+    if (view === 'all') {
+      return setBlogPostData({ posts: blogPosts, isLoaded: true })
+    }
     const latestReviews = [blogPosts[0], blogPosts[1], blogPosts[2]]
     setBlogPostData({ posts: latestReviews, isLoaded: true })
   }, [])
