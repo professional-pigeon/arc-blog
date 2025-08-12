aad3308a8aa085ecb8ad051ecc1a5c491940e86e
professional-pigeon <kyle.kayperez@gmail.com>
2025-08-12 15:18:18 -0700

    add color to rating

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import styles from './PostCard.module.css'
+import styles from './PostCard.module.css';
 
 export default function PostCard({ post }) {
   return (
@@ -8,10 +8,10 @@ export default function PostCard({ post }) {
         <p>-</p>
         <p>{post.date}</p>
         <p>-</p>
-        <p>{post.rating}</p>
+        <p className={styles.rating}>&#9733; {post.rating}/5</p>
       </div>
-      <h3><a href='/blog'>{post.title}</a></h3>
-      <p className={styles.review}>{post.review}/5</p>
+      <h3><a href={`/blog/${post.id}`}>{post.title}</a></h3>
+      <p className={styles.review}>{post.review}</p>
     </div>
   );
 };
