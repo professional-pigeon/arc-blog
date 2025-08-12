95bfec73f32412cdab72685ff4777e91553f65da
professional-pigeon <kyle.kayperez@gmail.com>
2025-08-12 14:27:21 -0700

    light styling to box and text components and add link to header

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -10,8 +10,8 @@ export default function PostCard({ post }) {
         <p>-</p>
         <p>{post.rating}</p>
       </div>
-      <h3>{post.title}</h3>
-      <p>{post.review}</p>
+      <h3><a href='/blog'>{post.title}</a></h3>
+      <p className={styles.review}>{post.review}/5</p>
     </div>
   );
 };
